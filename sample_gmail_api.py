bbe52a91d65ba56913da2c612614d17e0d29daea
FT-yuki <yakinikug1010@gmail.com>
2020-05-31 08:07:09 +0900

    Update sample_gmail.api.py

--- sample_gmail_api.py
+++ sample_gmail_api.py
@@ -27,48 +27,64 @@ class GmailAPI:
             userId='me', pageToken=nextPageToken).execute()
         return messageIDlist
 
-    def crowling_mail_box(self):
+    def crowling_mail_box(self, th):
         #APIに接続
         service = self.ConnectGmail()
-        MessageList = []
-        messageIDlist = self.GetMessageList(service)
-        nextPageToken = messageIDlist["nextPageToken"]
-        while nextPageToken:
-            #該当するメールが存在しない場合は、処理中断
-            if messageIDlist['resultSizeEstimate'] == 0:
-                print("Message is not found")
-                return MessageList
-            #メッセージIDを元に、メールの詳細情報を取得
-            for message in tqdm(messageIDlist['messages']):
-                row = {}
-                row['ID'] = message['id']
-                MessageDetail = service.users().messages().get(
-                    userId='me', id=message['id']).execute()
-                for header in MessageDetail['payload']['headers']:
-                    #日付、送信元、件名を取得する
-                    if header['name'] == 'Date':
-                        row['Date'] = header['value']
-                    elif header['name'] == 'From':
-                        row['From'] = header['value']
-                    elif header['name'] == 'Subject':
-                        row['Subject'] = header['value']
-                # 本文を取得
-                # import ipdb; ipdb.set_trace()
-                # base64_text = MessageDetail["payload"]["parts"][0]["data"]
-                # def base64_dencoder(text):
-                #     if len(text)>0:
-                #         message = base64.urlsafe_b64decode(text)
-                #         message = str(message, 'utf-8')
-                #         # message = email.message_from_string(message)
-                #     return message
-                # row["text"] = base64_dencoder(base64_text)
-                MessageList.append(row)
-            messageIDlist = self.GetMessageList(
-                service, messageIDlist["nextPageToken"])
-            nextPageToken = messageIDlist["nextPageToken"]
-            if len(MessageList) >= 100:
-                return MessageList
-        return MessageList
+        message_list = []
+        next_page_token = None
+        for i in tqdm(range(0, th)):
+            message_list_100 = self.get_message_list(service, next_page_token)
+            message_list.extend(message_list_100["message_list"])
+            if message_list_100["next_page_token"]:
+                    next_page_token = message_list_100["next_page_token"]
+            else:
+                break
+        return message_list
+
+    def get_message_list(self, service, next_page_tolen=None):
+        result = {"message_list": [], "next_page_token": None}
+        try:
+            if next_page_tolen:
+                messageIDlist = self.GetMessageList(service, next_page_tolen)
+            else:
+                messageIDlist = self.GetMessageList(service)                    
+            result["next_page_token"] = messageIDlist["nextPageToken"]                    
+        except:
+            print("API Error")
+            raise
+
+
+        #該当するメールが存在しない場合は、処理中断
+        if messageIDlist['resultSizeEstimate'] == 0:
+            print("Message is not found")
+            return result
+
+        #メッセージIDを元に、メールの詳細情報を取得
+        for message in tqdm(messageIDlist['messages']):
+            row = {}
+            row['ID'] = message['id']
+            MessageDetail = service.users().messages().get(
+                userId='me', id=message['id']).execute()
+            for header in MessageDetail['payload']['headers']:
+                #日付、送信元、件名を取得する
+                if header['name'] == 'Date':
+                    row['Date'] = header['value']
+                elif header['name'] == 'From':
+                    row['From'] = header['value']
+                elif header['name'] == 'Subject':
+                    row['Subject'] = header['value']
+            # 本文を取得
+            # import ipdb; ipdb.set_trace()
+            # base64_text = MessageDetail["payload"]["parts"][0]["data"]
+            # def base64_dencoder(text):
+            #     if len(text)>0:
+            #         message = base64.urlsafe_b64decode(text)
+            #         message = str(message, 'utf-8')
+            #         # message = email.message_from_string(message)
+            #     return message
+            # row["text"] = base64_dencoder(base64_text)
+            result["message_list"].append(row)
+        return result
 
 
 def to_dataframe_from_message(messages):
@@ -84,9 +100,6 @@ def to_dataframe_from_message(messages):
 
 if __name__ == '__main__':
     test = GmailAPI()
-    #パラメータは、任意の値を指定する
-    messages = test.crowling_mail_box()
+    messages = test.crowling_mail_box(10)
     messages_df = to_dataframe_from_message(messages)
-    #結果を出力
-    import ipdb
-    ipdb.set_trace()
+    messages_df.to_pickle("./crowling_data.pkl")
