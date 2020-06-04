843c8fc682a5d8767706be82cc8b728c2de4cb67
FT-yuki <yakinikug1010@gmail.com>
2020-06-05 05:52:10 +0900

    add func of exporting word_data.json

--- make_word_cloud.py
+++ make_word_cloud.py
@@ -3,6 +3,18 @@ import wordcloud
 import MeCab
 from itertools import chain
 from matplotlib.font_manager import FontProperties
+import unicodedata
+import string
+from collections import Counter, OrderedDict
+import codecs
+import json
+
+
+def format_text(text):
+    text = unicodedata.normalize("NFKC", text)  # 全角記号をざっくり半角へ置換（でも不完全）
+    table = str.maketrans("", "", string.punctuation  + "「」、。・【】")
+    text = text.translate(table)
+    return text
 
 
 if __name__ == "__main__":
@@ -12,9 +24,20 @@ if __name__ == "__main__":
     m = MeCab.Tagger('-Ochasen')    
     parsed = m.parse("".join(text))
     #助詞、助動詞を除いて単語結合
-    splitted = ' '.join([x.split('\t')[0] for x in parsed.splitlines()[
-                        :-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞']])
+    splitted = [x.split('\t')[0] for x in parsed.splitlines()[:-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞']]
+    splitted = [x for x in list(map(format_text, splitted)) if x is not ""]
+    word_data = Counter(splitted)
+    word_data_json = OrderedDict()
+    DROP_MIN_COUNT = 10
+    for values in word_data.most_common():
+        if values[1] > DROP_MIN_COUNT:
+            word_data_json[values[0]] = values[1]
 
+    JSON_FILE_NAME = 'word_data.json'
+    with open(JSON_FILE_NAME, 'w') as f:
+        json.dump(word_data_json, f, indent=4, ensure_ascii=False)
+
+    splitted = "".join(splitted)
     FONT_PATH = "./ipaexg.ttf"
     wordc = wordcloud.WordCloud(font_path=FONT_PATH).generate(splitted)
     wordc.to_file('sample-wordCloud-jpn.png')
