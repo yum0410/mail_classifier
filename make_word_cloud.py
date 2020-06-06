4f4c4ccc481a584768a2127f072f1aa685e727dc
FT-yuki <yakinikug1010@gmail.com>
2020-06-06 18:10:55 +0900

    update make_word_cloud.py choice word.

--- make_word_cloud.py
+++ make_word_cloud.py
@@ -21,15 +21,27 @@ if __name__ == "__main__":
     data = pd.read_pickle("./crowling_data.pkl")
     text = data["Subject"].values.tolist()
 
-    m = MeCab.Tagger('-Ochasen')    
-    parsed = m.parse("".join(text))
-    #助詞、助動詞を除いて単語結合
-    splitted = [x.split('\t')[0] for x in parsed.splitlines()[:-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞']]
-    splitted = [x for x in list(map(format_text, splitted)) if x is not ""]
-    word_data = Counter(splitted)
+    tokenizer = MeCab.Tagger('-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')    
+    parsed = tokenizer.parse("".join(text))
+
+    target_text = []
+    for sentence in text:
+        tokenizer.parse("")
+        node = tokenizer.parseToNode(sentence)
+        keywords = []
+        while node:
+            if node.feature.split(",")[0] == u"名詞":
+                keywords.append(node.surface)
+            elif node.feature.split(",")[0] == u"動詞":
+                keywords.append(node.feature.split(",")[6])
+            node = node.next
+        target_text.append(keywords)
+    target_text = list(chain.from_iterable(target_text))
+
+    word_data = Counter(target_text)
     word_data_json = OrderedDict()
     word_data_list = []
-    DROP_MIN_COUNT = 10
+    DROP_MIN_COUNT = 5
     for values in word_data.most_common():
         if values[1] > DROP_MIN_COUNT:
             word_data_list.append({"word": values[0], "count": values[1]})
@@ -37,7 +49,7 @@ if __name__ == "__main__":
     with open(JSON_FILE_NAME, 'w') as f:
         json.dump(word_data_list, f, indent=4, ensure_ascii=False)
 
-    splitted = "".join(splitted)
+    target_text = "".join(target_text)
     FONT_PATH = "./ipaexg.ttf"
-    wordc = wordcloud.WordCloud(font_path=FONT_PATH).generate(splitted)
+    wordc = wordcloud.WordCloud(font_path=FONT_PATH).generate(target_text)
     wordc.to_file('sample-wordCloud-jpn.png')
